2206b5a94c2b9d651765d67d73ed9d31ea49a6d0
Sarina Ocal <sarinaocal@gmail.com>
2020-06-17 01:32:32 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -5,4 +5,20 @@ puts "Hello #{name}." + " It's nice to meet you."
 
 end
 
+begin
+    return file_scope.local_variable_get(variable)
+  rescue NameError
+    raise NameError, "local variable `#{variable}' not defined in #{file}."
+  end    
+end
 
+def capture_puts
+  begin
+    old_stdout = $stdout
+    $stdout = StringIO.new('','w')
+    yield
+    $stdout.string
+  ensure
+    $stdout = old_stdout
+  end
+end
