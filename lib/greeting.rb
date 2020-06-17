2dcbcb0f7de55a998acb49417aca74f570a90e0c
Sarina Ocal <sarinaocal@gmail.com>
2020-06-17 01:38:02 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,9 +1,9 @@
 # code the #greeting method here!
 #def greeting(name)
 
-puts "Hello #{name}." + " It's nice to meet you."
+#puts "Hello #{name}." + " It's nice to meet you."
 
-end
+#end
 
 def
  
